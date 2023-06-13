9ad57320ffb72cd9cc9fabbbc5c2dcab230e215e
lcesarfs <lcesarfs@hotmail.com>
2023-06-13 22:19:23 +0100

    versao com gpio funcionando clean

--- Debug/Src/001led_toggle.d
+++
@@ -1,7 +0,0 @@
-Src/001led_toggle.o: ../Src/001led_toggle.c \
- C:/stm32prjs/drivers/stm32Drivers/stm32Drivers/drivers/Inc/stm32f106cbt6.h \
- C:/stm32prjs/drivers/stm32Drivers/stm32Drivers/drivers/Inc/stm32f106_gpio.h \
- C:/stm32prjs/drivers/stm32Drivers/stm32Drivers/drivers/Inc/stm32f106cbt6.h
-C:/stm32prjs/drivers/stm32Drivers/stm32Drivers/drivers/Inc/stm32f106cbt6.h:
-C:/stm32prjs/drivers/stm32Drivers/stm32Drivers/drivers/Inc/stm32f106_gpio.h:
-C:/stm32prjs/drivers/stm32Drivers/stm32Drivers/drivers/Inc/stm32f106cbt6.h:
--- Debug/drivers/src/stm32f106_gpio.d
+++
@@ -1,7 +0,0 @@
-drivers/src/stm32f106_gpio.o: ../drivers/src/stm32f106_gpio.c \
- C:/stm32prjs/drivers/stm32Drivers/stm32Drivers/drivers/Inc/stm32f106_gpio.h \
- C:/stm32prjs/drivers/stm32Drivers/stm32Drivers/drivers/Inc/stm32f106cbt6.h \
- C:/stm32prjs/drivers/stm32Drivers/stm32Drivers/drivers/Inc/stm32f106_gpio.h
-C:/stm32prjs/drivers/stm32Drivers/stm32Drivers/drivers/Inc/stm32f106_gpio.h:
-C:/stm32prjs/drivers/stm32Drivers/stm32Drivers/drivers/Inc/stm32f106cbt6.h:
-C:/stm32prjs/drivers/stm32Drivers/stm32Drivers/drivers/Inc/stm32f106_gpio.h:
